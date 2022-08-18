370bc9717c3964af0d3d8c4223610f5072ef5622
Gokul Kumar Parmar <parmarhappy191@gmail.com>
2022-08-18 18:04:42 +0530

    Initial Commit

---
+++ HappyCard/HappyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyCardApp.swift
+//  HappyCard
+//
+//  Created by gokulparmar on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HappyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
